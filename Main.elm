bde808ce0b5786ecd06a834a0d192a71ba36d9a7
Aaron VonderHaar <gruen0aermel@gmail.com>
2015-09-01 19:55:44 -0700

    Add Main.elm visualization

---
+++ Main.elm
@@ -0,0 +1,23 @@
+module Main where
+
+import Protocol exposing (..)
+import Table
+
+config =
+    { foodResponse = linear 2 }
+
+data =
+    [ Food (time  9) 50
+    , Food (time 11) 15
+    , Food (time 12) 50
+    ]
+
+main =
+    let
+        p i = process config data (time i) (time <| i+1)
+    in
+        [0..24]
+        |> Table.table
+            [ ("Time", toString)
+            , ("Food", p >> .food >> toString)
+            ]
